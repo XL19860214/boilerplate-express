7014ae5dc619b7f75cd2a28a2b90c0062733aeb9
Xuwei Li <72472538+XL19860214@users.noreply.github.com>
2021-08-20 09:39:00 +0000

    Respond for route /json.

--- myApp.js
+++ myApp.js
@@ -14,7 +14,11 @@ app.get('/', (req, res) => {
 
 app.use('/public', express.static(__dirname + '/public'));
 
-
+app.get('/json', (req, res) => {
+  res.json({
+    message: 'Hello json'
+  })
+})
 
 
 
